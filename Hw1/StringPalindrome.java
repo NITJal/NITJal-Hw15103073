e2626de01adb18e2bbf20a879de7aeaf59c311cf
Shailender1911 <30435886+Shailender1911@users.noreply.github.com>
2017-12-02 20:28:04 +0530

    Removed White spaces

--- Hw1/StringPalindrome.java
+++ Hw1/StringPalindrome.java
@@ -1,7 +1,5 @@
 package assignment;
-
 import java.util.Scanner;
-
 public class StringPalindrome {
 	public static void main(String args[]) {
         String s ;
@@ -10,18 +8,12 @@ public class StringPalindrome {
         s = sc.nextLine();
         String reverse = "";
         int len = s.length();
-
-        for (int i = len - 1; i > -1; i--) {
-            reverse = reverse + s.charAt(i);
-        }
+		for (int i = len - 1; i > -1; i--) {
+            reverse = reverse + s.charAt(i);}
         System.out.println("Reversed String is: " + reverse);
-
-        if (s.equals(reverse)) {
-            System.out.println("The Given String is Palindrome");
-        } else {
-            System.out.println("The Given String is not a Palindrome");
-        }
-       sc.close();
-    }
-}
+	if (s.equals(reverse)) {
+            System.out.println("The Given String is Palindrome");} 
+		else {
+            System.out.println("The Given String is not a Palindrome");}
+       sc.close();}}
 
